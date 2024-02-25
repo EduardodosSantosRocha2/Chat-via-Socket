073737afc7c761db0ebdfcb0237b23791fab0558
Eduardo dos Santos <duduredtone@gmail.com>
2024-02-25 19:40:24 -0300

    chat_via_socket

---
+++ chatSocketSD/src/main/java/com/mycompany/chatsocketsd/ChatSocketSD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatsocketsd;
+
+/**
+ *
+ * @author dudur
+ */
+public class ChatSocketSD {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
